e429412c8a77e5cdc30e4ed7d575dbdaf01ae845
ol3gatron <olegatron@gmail.com>
2022-11-08 23:57:04 +0300

    created mobile layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import HeaderIcon from "../../assets/icons/Vector.svg"
 import HeaderText from "../../assets/icons/Circeya.svg"
+import PhoneIcon from "../../assets/icons/PhoneIcon.svg"
 import "./Header.css"
 
 const Header = () => {
@@ -9,9 +10,9 @@ const Header = () => {
         <img src={HeaderIcon} alt="Icon" />
         <img src={HeaderText} alt="Circeya" />
       </div>
-      <p className="header-phone">
-        +7 (495) 495-49-54
-      </p>
+      <div className="header-phone">
+        <img src={PhoneIcon} alt="Call" />
+      </div>
     </header>
   )
 }
